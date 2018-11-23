4c212e6ad63cbd392e0fdf5b7a98d7b835a482c8
winnie <wangliu023@qq.com>
2018-11-23 11:25:16 +0800

    sync

--- app/src/main/java/com/winnie/utils/statusbarutlils/StatusBarActivity5.java
+++ app/src/main/java/com/winnie/utils/statusbarutlils/StatusBarActivity5.java
@@ -20,6 +20,9 @@ public class StatusBarActivity5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_bar5);
+
+        //1、LAYOUT_FULLSCREEN：使状态栏出现的时候，不会重新调整activity的高度，状态栏覆盖在activity之上。
+        //2、LAYOUT_FULLSCREEN 、LAYOUT_STABLE：让应用的主体内容占用系统状态栏的空间；
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         mRootView = findViewById(R.id.layout);
